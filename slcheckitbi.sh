e8e2e4b9317b0a7aff5eb75bb3965beffc643ea2
gallgeorge53-boop <gallgeorge53@gmail.com>
2025-12-30 17:59:22 +0200

    In acest commit am creat o functie iterativa de parcurgere a directoarelor si am corectat o greseala de sintaxa din commitul anterior

--- slcheckitbi.sh
+++ slcheckitbi.sh
@@ -1,4 +1,4 @@
-#!/bin/bash/
+#!/bin/bash
 
 director="$1"
 
@@ -6,3 +6,14 @@ if [ ! -d "$director" ]; then
 echo "$director nu este un director"
 exit 1
 fi
+
+parcurgere_directoare() {
+director_curent="$1"
+for obiect in "director_curent"/*; do
+  [ -e "$obiect" ] || continue 
+
+  if  [-d "$obiect" ]; then
+     parcurgere_directoare "$obiect"
+  fi
+done
+}
