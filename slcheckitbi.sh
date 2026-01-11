2b16257739bdf96274ac4af361c842034bdc110f
gallgeorge53-boop <gallgeorge53@gmail.com>
2026-01-11 14:55:44 +0200

    In acest commit am adaugat o comanda prin care se evita pierderea symlinkurilor broken si o alta conditie prin care evitam buclele infinite din recursivitate

--- slcheckitbi.sh
+++ slcheckitbi.sh
@@ -8,16 +8,15 @@ exit 1
 fi
 
 parcurgere_directoare() {
-director_curent="$1"
+local director_curent="$1"
 for obiect in "$director_curent"/*; do
-  [ -e "$obiect" ] || continue 
+  [ -e "$obiect" ] || [ -L "$obiect"] ||  continue 
    if [ -L "$obiect" ]; then
 	if [ ! -e "$obiect" ]; then
-		echo " $obiect  este un broken link"
+		echo " $obiect este un broken link"
 	fi
-   fi
 
-  elif  [-d "$obiect" ]; then
+  elif  [ -d "$obiect" ] &&  [ ! -L "$obiect" ]; then
      parcurgere_directoare "$obiect"
   fi
 done
