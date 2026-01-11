2cd4652efff09a655bc8a71d303ca8d387ddf737
gallgeorge53-boop <gallgeorge53@gmail.com>
2026-01-11 20:01:55 +0200

    In acest commit am rezolvat si restul edgecase-urilor si am adaugat un mesaj de final in caz ca directorul nu are niciun 'broken link'

--- slcheckitbi.sh
+++ slcheckitbi.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
 
+shopt -s dotglob
+
 director="$1"
 
 if [ ! -d "$director" ]; then 
 echo "$director nu este un director"
 exit 1
 fi
-
+ok=1
 parcurgere_directoare() {
 local director_curent="$1"
 for obiect in "$director_curent"/*; do
-  [ -e "$obiect" ] || [ -L "$obiect"] ||  continue 
-   if [ -L "$obiect" ]; then
+if [ -d "$obiect" ] && ( [ ! -r "$obiect" ] || [ ! -x "$obiect" ] );then
+	echo "$obiect nu are permisiuni de citire sau executare"
+	continue
+fi
+  [ -e "$obiect" ] || [ -L "$obiect" ] ||  continue
+ 
+  if [ -L "$obiect" ]; then
 	if [ ! -e "$obiect" ]; then
 		echo " $obiect este un broken link"
+		ok=0
 	fi
 
   elif  [ -d "$obiect" ] &&  [ ! -L "$obiect" ]; then
@@ -22,3 +30,6 @@ for obiect in "$director_curent"/*; do
 done
 }
 parcurgere_directoare "$director"
+if(( ok==1 )); then
+echo "Acest director nu are 'broken links'"
+fi
